d5bbd4e082609c7b7fc14b7ea7fd53e0e1324259
Tushar <tusharsharma@virtualemployee.com>
2023-05-02 11:51:17 +0530

    "Typing "

--- Python-Projects/speed_calculator.py
+++ Python-Projects/speed_calculator.py
@@ -16,16 +16,23 @@ def speed_time(time_s, time_e, userinput):
     time_R = round(time_delay,2)
     speed = len(userinput)/time_R
     return round(speed) 
-
-test = ["In this typing speed test game using Python ","tutorial, you will learn how to check the speed.", " If you know the basics of Python and the pygame library, you can easily calculate the speed"]
-
-test1 = r.choice(test)
-print("  ******Typing speed*********  ")
-print(test1)
-print()
-print()
-time_1 = time()
-testinput = input("Enter: ")
-time_2 = time()
-print('Speed : ' ,speed_time(time_1, time_2, testinput), "w/sec")
-print("Error : ",mistake(test1, testinput))
+while True:
+    ck = input("Ready to test your Speed:")
+    if ck == "yes":
+          test = ["In this typing speed test game using Python ","tutorial, you will learn how to check the speed.", " If you know the basics of Python and the pygame library, you can easily calculate the speed"]
+          test1 = r.choice(test)
+          print("  ******Typing speed*********  ")
+          print(test1)
+          print()
+          print()
+          time_1 = time()
+          testinput = input("Enter: ")
+          time_2 = time()
+          print('Speed : ' ,speed_time(time_1, time_2, testinput), "w/sec")
+          print("Error : ",mistake(test1, testinput))
+    if ck == "no":
+        print("THank You for your time")
+        break
+    
+    else:
+        print("Please Enter the valid input")
